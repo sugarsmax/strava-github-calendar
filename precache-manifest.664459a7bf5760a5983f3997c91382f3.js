080e21d96eac26eb374b347a6c267f8abd913f4b
Pete Maxfield <peter.maxfield@autodesk.com>
2025-06-22 17:27:46 -0700

    Updates

---
+++ precache-manifest.664459a7bf5760a5983f3997c91382f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9594ab1979f490261349c35a5e8570c",
+    "url": "/strava-github-calendar/index.html"
+  },
+  {
+    "revision": "4c8e2f541b57419bc139",
+    "url": "/strava-github-calendar/static/css/main.f071b5a5.chunk.css"
+  },
+  {
+    "revision": "7cafeb59bad75791d96b",
+    "url": "/strava-github-calendar/static/js/2.441e5988.chunk.js"
+  },
+  {
+    "revision": "06ba7f95c174e9291d6759b19e6f2234",
+    "url": "/strava-github-calendar/static/js/2.441e5988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c8e2f541b57419bc139",
+    "url": "/strava-github-calendar/static/js/main.23e4c4a5.chunk.js"
+  },
+  {
+    "revision": "12bc03faf98ad3aca01c",
+    "url": "/strava-github-calendar/static/js/runtime-main.abbb06d3.js"
+  },
+  {
+    "revision": "52946ee09de928259805f841e8ef95cd",
+    "url": "/strava-github-calendar/static/media/logo.52946ee0.png"
+  }
+]);
